6d05bd603cad56f127b72ddc88c4854c55fc4688
linhheo <tlinhhpu2@gmail.com>
2019-09-17 20:53:41 +0700

    Chapter 3 - Mostly static pages

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  # trường name không được bỏ trống
-  validates :name, presence: true
-  # trường email không được bỏ trống
-  validates :email, presence: true
-  # Quan hệ 1- N : 1 user có nhiều micropost
-  has_many :microposts
-end
