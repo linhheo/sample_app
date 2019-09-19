0b8ef5052a0fa6909e32ce9fb7873c2b7a743ce2
Heo <tlinhhpu2@gmail.com>
2019-09-19 11:51:21 +0700

    Chapter 13 - User microposts
    Chapter 13 - User microposts
    
    
    Chap 13
    
    
    chap 13
    
    
    chap 13 u
    
    
    Chap 13

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,8 @@ def index
 end
 
 def show
-	@user = User.find(params[:id])
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
 end
 	
 def new
@@ -48,10 +49,10 @@ end
 
 private
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,:password_confirmation)
-    end
-
+	def user_params
+		params.require(:user).permit(:name, :email, :password,
+									 :password_confirmation)
+	  end
 	def logged_in_user
 	  unless logged_in?
 	    store_location
@@ -61,11 +62,11 @@ private
 	end
 
     def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url) unless current_user?(@user)
-    end
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
+	end
 
     def admin_user
-  		redirect_to(root_url) unless current_user.admin?
+		redirect_to(root_url) unless current_user.admin?
 	end
 end
